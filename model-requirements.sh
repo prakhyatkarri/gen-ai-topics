80780dfffec5ea5aeec425c401a3b984233c3233
P K <prakhyatkarri@gmail.com>
2026-06-28 14:00:26 -0400

    Databricks changes

---
+++ model-requirements.sh
@@ -0,0 +1,7 @@
+# uv pip install torch --index-url https://download.pytorch.org/whl/cpu
+uv pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0
+uv pip install transformers==5.0.0
+uv pip install datasets 
+uv pip install evaluate 
+uv pip install accelerate 
+uv pip install timm
